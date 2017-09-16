79a895418a75dfb6a3cb187851b650856d46333c
qiuyd_home <984175512@qq.com>
2017-09-16 16:17:39 +0800

    基本完成，下班，保存一下

--- app/src/main/java/com/example/qiuyd/tradedemo/view/WuxiPage.java
+++ app/src/main/java/com/example/qiuyd/tradedemo/view/WuxiPage.java
@@ -2,7 +2,9 @@ package com.example.qiuyd.tradedemo.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.example.qiuyd.tradedemo.R;
 
@@ -15,5 +17,10 @@ public class WuxiPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wuxi_page);
         webView = (WebView)findViewById(R.id.wuxi);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://www.baidu.com");
+
     }
 }
